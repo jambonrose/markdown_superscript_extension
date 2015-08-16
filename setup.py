a69fa698eb805bc12ad45cc2fa2ab9496e7bc125
Andrew Pinkham <code@andrewsforge.com>
2015-08-15 19:56:34 -0500

    Add implementation metadata to PyPI.

--- setup.py
+++ setup.py
@@ -54,6 +54,8 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Filters',
