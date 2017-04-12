c69b847064d1421ddff0b7dcae772b9bfa159a7d
Andrew Pinkham <code@andrewsforge.com>
2017-04-12 18:37:59 -0400

    Update Python and Markdown versions.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     url='https://github.com/jambonrose/markdown_superscript_extension',
 
     py_modules=['mdx_superscript'],
-    install_requires=['Markdown>=2.4'],
+    install_requires=['Markdown>=2.5,<2.7'],
 
     license='Simplified BSD License',
     classifiers=[
@@ -48,15 +48,14 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Filters',
         'Topic :: Text Processing :: Markup :: HTML',
